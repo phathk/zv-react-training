91b87a39181c5ff4e261fac57172f940b731cdb8
HKP <kimphat.devcs@gmail.com>
2021-07-18 18:56:24 +0700

    Update

--- Training6/Task/src/redux/sagas/rootSaga.js
+++
@@ -1,5 +0,0 @@
-import { all } from "redux-saga/effects";
-import * as task from "./taskSaga";
-export default function* rootSaga() {
-    yield all([task.taskSaga()]);
-}
--- Training6/Task/src/redux/state/state.js
+++
@@ -1,4 +0,0 @@
-export const initialState = {
-    task: [],
-    channelStatus: window.navigator.onLine,
-};
